c013869d36bb7db2abf17a3e615ac63b71804dd0
Guilherme Cintra (gcintra2) <gcintra2@na.xom.com>
2020-05-24 09:46:21 -0300

    extending newly created trait to magically parse amountPayload wire type within the GenerealExpense

--- app/general_expense/payload/GeneralExpenseInput.scala
+++ app/general_expense/payload/GeneralExpenseInput.scala
@@ -4,13 +4,14 @@ import clt_contract.payloads.OccurrencesPayload
 import general_expense.payload.GeneralExpenseInput.ExpenseType
 import play.api.libs.json.{Json, Reads}
 import wire.AmountPayload
+import wire.AmountPayload.AmountPayloadImplicits
 
 case class GeneralExpenseInput(amount: AmountPayload,
                                occurrences: OccurrencesPayload,
                                expenseType: Option[ExpenseType],
                                fixedAmount: Option[Boolean])
 
-object GeneralExpenseInput {
+object GeneralExpenseInput extends AmountPayloadImplicits {
 
   implicit val generalExpenseReads: Reads[GeneralExpenseInput] = Json.reads[GeneralExpenseInput]
 
